394f348cc783d6c2adcd1a0993b3b7880d99b792
Shreya Shukla <shreyashukla20042005@gmail.com>
2025-08-05 15:26:27 +0530

    Serve new

--- Backend/server.js
+++ Backend/server.js
@@ -1,14 +1,15 @@
-const app = require('./app');
-const mongoose = require('mongoose');
-const dotenv = require('dotenv');
+const app = require("./app");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
 
-dotenv.config({ path: __dirname + '/../.env' });
+dotenv.config();
 
 const PORT = process.env.PORT || 5000;
 
-mongoose.connect(process.env.MONGO_URI)
+mongoose
+  .connect(process.env.MONGO_URI)
   .then(() => {
     console.log("MongoDB connected");
     app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
   })
-  .catch(err => console.error("MongoDB connection error:", err));
+  .catch((err) => console.error("MongoDB connection error:", err));
