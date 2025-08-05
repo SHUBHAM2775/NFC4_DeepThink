5141d280d341ab6c65b8c944066f786b267fa68a
Shreya Shukla <shreyashukla20042005@gmail.com>
2025-08-05 17:12:39 +0530

    login and registartion complete

--- Backend/app.js
+++ Backend/app.js
@@ -1,13 +1,24 @@
 const express = require("express");
 const cors = require("cors");
 
-const authRoutes = require("./routes/authRoutes");
+console.log("✅ app.js loaded");
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
+
+const registerRoutes = require("./routes/registerRoutes");
+app.use("/api/register", registerRoutes);
+
+
+const authRoutes = require("./routes/authRoutes");
 app.use("/api/auth", authRoutes);
+
+
+const voiceLogRoutes = require("./routes/voiceLogRoutes");
+app.use("/api/voice", voiceLogRoutes);
+
 
 module.exports = app;
--- Backend/server.js
+++ Backend/server.js
@@ -5,6 +5,7 @@ const dotenv = require("dotenv");
 dotenv.config();
 
 const PORT = process.env.PORT || 5000;
+console.log("✅ server.js loaded");
 
 mongoose
   .connect(process.env.MONGO_URI)
